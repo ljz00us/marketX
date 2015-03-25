6af8eb0a7edfe7411ff946971e2df4e45830f1c1
Greg Levin <ljz00us@gmail.com>
2015-03-24 18:59:43 -0700

    v2 with early edu platform

--- javascript.js
+++ javascript.js
@@ -1,5 +1,17 @@
 
+$(document).ready () {
 
-function trade () {
-	alert("we're still working on that");
+	$("#congrats").hide();
+	
+	function trade () {
+		alert("we're still working on that");
+	}
+
+	function guess_check () {
+		var guess = document.getElementById("guess").value;
+		if (guess == "5") {
+			document.getElementById("congrats").style.display = "block";
+		} 
+	}
+
 }
